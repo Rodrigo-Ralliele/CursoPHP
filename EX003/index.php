f657d96145e4c38dc3089dced988fb4e1e4134c5
Rodrigo Ralliele <146042299+Rodrigo-Ralliele@users.noreply.github.com>
2023-11-26 21:26:22 -0300

    ex003

---
+++ EX003/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php
+        // // 0x = hexadecimal 0b = binario 0= Octal 
+        // $num = 0x1A;
+        // echo "O valor da variavel é $num"
+        
+        // $v = "Ralliele";
+        // var_dump($v);
+
+        // $num = (int)3e2; // 3 x 10(2) coerção
+        // // echo "O valor é $num";
+        // var_dump($num);
+
+        // $num = (float)"950";
+        // var_dump($num);
+
+        $casado = True;
+        // var_dump($casado)
+        echo "O valor para casado é $casado"
+
+    ?>
+</body>
+
+</html>

44ea578648073deb1c7c67e95285f6906763b17b
Rodrigo Ralliele <146042299+Rodrigo-Ralliele@users.noreply.github.com>
2023-11-27 23:42:42 -0300

    aULA 22

---
+++ EX004/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste De Strings</title>
+</head>
+<body>
+    <H1>Strings Test </H1>
+    <?php 
+    // $nome =  "Rodrigo";
+    // $sobrenome = "Ralliele";
+    
+    // echo "$nome $sobrenome \u{1F596}";
+
+    // const Estado = "GO";
+    // echo "Eu adoro morar em " . Estado;
+
+    // const CANAL  = "Drkkkkj";
+    // echo "Me siga no Canal ". CANAL
+    // w
+    // $nom = "Rodrigo";
+    // $snom = "Nogueira";
+    // echo "$nom \"Minotauro\" $snom";
+    // $nom = "Rodrigo";
+    // $snom  = "Ralliele"; 
+    // $apelido = "Janela";
+
+    // echo "$nom \t\"$apelido\"\t $snom";
+    $canal = "Curso em Video";
+    $ano = date('Y');
+    echo <<< 'TESTE'
+    Olá Galera do $canal
+            Tudo bem com vocês?
+                Como está sendo o $ano de Vocês?
+                TESTE;
+    ?>
+</body>
+</html> 

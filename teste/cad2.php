14a85d516412b48428c78e865226b77150d8281d
Rodrigo Ralliele <146042299+Rodrigo-Ralliele@users.noreply.github.com>
2023-12-02 21:16:11 -0300

    treino

---
+++ teste/cad2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soma e spnan</title>
+</head>
+<body>
+    <?php 
+    $n1 = $_GET ["number"]; 
+    $n2 = $_GET ["numbertwo"];
+    $res = $n1 -  $n2 ; 
+    echo "A soma dos Numeros descritos é igual a $res"
+    ?>
+</body>
+</html>

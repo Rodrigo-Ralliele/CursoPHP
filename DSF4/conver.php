7771ffdbc5bf8de14ef5348034021b680aa645f0
Rodrigo Ralliele <146042299+Rodrigo-Ralliele@users.noreply.github.com>
2023-12-03 11:03:34 -0300

    dsf4

---
+++ DSF4/conver.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <style>
+        header > h1 {
+            color: black;
+            text-shadow: 0px 0px 0px black;;
+        }
+    </style>
+</head>
+<body>
+ <main>
+     <header>
+        <h1>Aalisador de Número Real</h1>
+     </header>
+     \
+ </main>
+</body>
+</html>
